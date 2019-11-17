04e9edfafa777968bf035db94aedab3fd5be18db
lz <qinlz@wenxiaoyou.com>
2019-11-17 19:11:44 +0800

    修改数据库初始化脚本

--- src/test/resources/db/init.sql
+++ src/test/resources/db/init.sql
@@ -1,67 +1,11 @@
-create database bnibt default character set utf8mb4 collate utf8mb4_general_ci;
-create user 'bnibt_user'@'%' identified by 'Bnibt@)!*';
-grant all privileges on bnibt.* to 'bnibt_user'@'%';
+create database jianxun default character set utf8mb4 collate utf8mb4_general_ci;
+create user 'user_lz'@'%' identified by 'lz666666';
+grant all privileges on bnibt.* to 'user_lz'@'%';
 flush  privileges;
 
 -- ----------------------------
 --  系统初始化数据
 -- ----------------------------
 BEGIN;
-INSERT INTO `staff` VALUES ('staff_root_id', 'ROOT', 'root', '1f0c6a3ff04c9f9e806f183e8230897e', 'dc78e5ac14104828a5acf7074b7e905b', '13520664663', 'root@126.com', null, null, null, '', '', '', 0, 0, 0, null, null, '', 0,'2018-09-08 15:19:13', '2018-09-08 15:19:13', 0, null);
-INSERT INTO `role` VALUES ('role_root_id', 'ROOT', '超级管理员', '2018-08-30 11:55:19', '2018-08-30 11:55:19', 0);
-INSERT INTO `role_permission` VALUES ('role_root_id', '1', '2018-09-07 14:29:22');
-INSERT INTO `permission` VALUES ('1', '', 'none', '数据看板', '', '2018-08-20 14:29:54', '2018-08-10 14:29:54', '0');
-
---教材版本父类型
-insert into `dictionary` ( `dic_key`, `parent_key`, `name`, `en_name`,`data`, `sort_order`, `created_time`, `updated_time`, `deleted`, `version`, `operator`)
-  values ( 'TEACH_MATERIAL', null, '教材', null, null, 0, '2017-08-05 22:54:07', '2017-08-05 22:54:07', 0, 1, 'administrator');
-
---教材科目父类型
-insert into `dictionary` ( `dic_key`, `parent_key`, `name`, `en_name`,`data`, `sort_order`, `created_time`, `updated_time`, `deleted`, `version`, `operator`)
-  values ( 'TEACH_SUBJECT', null, '科目', null, null, 0, '2017-08-05 22:54:07', '2017-08-05 22:54:07', 0, 1, 'administrator');
-
---视频艺术形式
-insert into `dictionary` ( `dic_key`, `parent_key`, `name`, `en_name`,`data`, `sort_order`, `created_time`, `updated_time`, `deleted`, `version`, `operator`)
-  values ( 'TEACH_ART_TYPES', null, '艺术类型', null, null, 0, '2017-08-05 22:54:07', '2017-08-05 22:54:07', 0, 1, 'administrator');
-
---所有教材版本
-insert into `dictionary` ( `dic_key`, `parent_key`, `name`, `en_name`,`data`, `sort_order`, `created_time`, `updated_time`, `deleted`, `version`, `operator`)
-  values ( 'TEACH_MATERIAL_BUBIAN', 'TEACH_MATERIAL', '部编版', null, null, '0', '2017-08-05 22:54:07', '2017-08-05 22:54:07', 0, 1, 'administrator');
-insert into `dictionary` ( `dic_key`, `parent_key`, `name`, `en_name`,`data`, `sort_order`, `created_time`, `updated_time`, `deleted`, `version`, `operator`)
-  values ( 'TEACH_MATERIAL_RENJIAO', 'TEACH_MATERIAL', '人教版', null, null, '0', '2017-08-05 22:54:07', '2017-08-05 22:54:07', 0, 1, 'administrator');
-insert into `dictionary` ( `dic_key`, `parent_key`, `name`, `en_name`,`data`, `sort_order`, `created_time`, `updated_time`, `deleted`, `version`, `operator`)
-  values ( 'TEACH_MATERIAL_YUWEN', 'TEACH_MATERIAL', '语文版', null, null, '0', '2017-08-05 22:54:07', '2017-08-05 22:54:07', 0, 1, 'administrator');
-insert into `dictionary` ( `dic_key`, `parent_key`, `name`, `en_name`,`data`, `sort_order`, `created_time`, `updated_time`, `deleted`, `version`, `operator`)
-  values ( 'TEACH_MATERIAL_SUJIAO', 'TEACH_MATERIAL', '苏教版', null, null, '0', '2017-08-05 22:54:07', '2017-08-05 22:54:07', 0, 1, 'administrator');
-insert into `dictionary` ( `dic_key`, `parent_key`, `name`, `en_name`,`data`, `sort_order`, `created_time`, `updated_time`, `deleted`, `version`, `operator`)
-  values ( 'TEACH_MATERIAL_LUJIAO', 'TEACH_MATERIAL', '鲁教版', null, null, '0', '2017-08-05 22:54:07', '2017-08-05 22:54:07', 0, 1, 'administrator');
-insert into `dictionary` ( `dic_key`, `parent_key`, `name`, `en_name`,`data`, `sort_order`, `created_time`, `updated_time`, `deleted`, `version`, `operator`)
-  values ( 'TEACH_MATERIAL_BEISHIDA', 'TEACH_MATERIAL', '北师大版', null, null, '0', '2017-08-05 22:54:07', '2017-08-05 22:54:07', 0, 1, 'administrator');
-insert into `dictionary` ( `dic_key`, `parent_key`, `name`, `en_name`,`data`, `sort_order`, `created_time`, `updated_time`, `deleted`, `version`, `operator`)
-  values ( 'TEACH_MATERIAL_HUJIAO', 'TEACH_MATERIAL', '沪教版', null, null, '0', '2017-08-05 22:54:07', '2017-08-05 22:54:07', 0, 1, 'administrator');
-insert into `dictionary` ( `dic_key`, `parent_key`, `name`, `en_name`,`data`, `sort_order`, `created_time`, `updated_time`, `deleted`, `version`, `operator`)
-  values ( 'TEACH_MATERIAL_JIJIAO', 'TEACH_MATERIAL', '冀教版', null, null, '0', '2017-08-05 22:54:07', '2017-08-05 22:54:07', 0, 1, 'administrator');
-insert into `dictionary` ( `dic_key`, `parent_key`, `name`, `en_name`,`data`, `sort_order`, `created_time`, `updated_time`, `deleted`, `version`, `operator`)
-  values ( 'TEACH_MATERIAL_ZHEJIAO', 'TEACH_MATERIAL', '浙教版', null, null, '0', '2017-08-05 22:54:07', '2017-08-05 22:54:07', 0, 1, 'administrator');
-insert into `dictionary` ( `dic_key`, `parent_key`, `name`, `en_name`,`data`, `sort_order`, `created_time`, `updated_time`, `deleted`, `version`, `operator`)
-  values ( 'TEACH_MATERIAL_HEDA', 'TEACH_MATERIAL', '河大版', null, null, '0', '2017-08-05 22:54:07', '2017-08-05 22:54:07', 0, 1, 'administrator');
-
---所有科目
-insert into `dictionary` ( `dic_key`, `parent_key`, `name`, `en_name`,`data`, `sort_order`, `created_time`, `updated_time`, `deleted`, `version`, `operator`)
-  values ( 'TEACH_SUBJECT_YUWEN', 'TEACH_SUBJECT', '语文', null, null, '0', '2017-08-05 22:54:07', '2017-08-05 22:54:07', 0, 1, 'administrator');
-insert into `dictionary` ( `dic_key`, `parent_key`, `name`, `en_name`,`data`, `sort_order`, `created_time`, `updated_time`, `deleted`, `version`, `operator`)
-  values ( 'TEACH_SUBJECT_SHUXUE', 'TEACH_SUBJECT', '数学', null, null, '0', '2017-08-05 22:54:07', '2017-08-05 22:54:07', 0, 1, 'administrator');
-
---所有艺术类型
-insert into `dictionary` ( `dic_key`, `parent_key`, `name`, `en_name`,`data`, `sort_order`, `created_time`, `updated_time`, `deleted`, `version`, `operator`)
-  values ( 'TEACH_ART_TYPES_DONGHUA', 'TEACH_ART_TYPES', '动画', null, null, '0', '2017-08-05 22:54:07', '2017-08-05 22:54:07', 0, 1, 'administrator');
-insert into `dictionary` ( `dic_key`, `parent_key`, `name`, `en_name`,`data`, `sort_order`, `created_time`, `updated_time`, `deleted`, `version`, `operator`)
-  values ( 'TEACH_ART_TYPES_SHUICAI', 'TEACH_ART_TYPES', '水彩', null, null, '0', '2017-08-05 22:54:07', '2017-08-05 22:54:07', 0, 1, 'administrator');
-insert into `dictionary` ( `dic_key`, `parent_key`, `name`, `en_name`,`data`, `sort_order`, `created_time`, `updated_time`, `deleted`, `version`, `operator`)
-  values ( 'TEACH_ART_TYPES_GUOHUA', 'TEACH_ART_TYPES', '国画', null, null, '0', '2017-08-05 22:54:07', '2017-08-05 22:54:07', 0, 1, 'administrator');
-insert into `dictionary` ( `dic_key`, `parent_key`, `name`, `en_name`,`data`, `sort_order`, `created_time`, `updated_time`, `deleted`, `version`, `operator`)
-  values ( 'TEACH_ART_TYPES_PIYING', 'TEACH_ART_TYPES', '皮影', null, null, '0', '2017-08-05 22:54:07', '2017-08-05 22:54:07', 0, 1, 'administrator');
-insert into `dictionary` ( `dic_key`, `parent_key`, `name`, `en_name`,`data`, `sort_order`, `created_time`, `updated_time`, `deleted`, `version`, `operator`)
-  values ( 'TEACH_ART_TYPES_CAIQIAN', 'TEACH_ART_TYPES', '彩铅', null, null, '0', '2017-08-05 22:54:07', '2017-08-05 22:54:07', 0, 1, 'administrator');
 
 COMMIT;
--- src/test/resources/db/trigger.sql
+++ src/test/resources/db/trigger.sql
@@ -3,6 +3,7 @@ DELIMITER $
 -- ----------------------------
 --  TRIGGER `insertUpdateStaffTalkNum`
 --  增加销售人员沟通客户数量用于公平分配客户咨询
+--  示例
 -- ----------------------------
 DROP TRIGGER IF EXISTS `insertUserCoinItem`;
 CREATE TRIGGER insertUserCoinItem
@@ -14,61 +15,5 @@ INSERT INTO user_coin(`id`, `user_id`, `mobile`, `coin_num`, `updated_time`, `op
     values(REPLACE(uuid(), '-', ''), new.id, new.phone, 0, CURRENT_TIME(), 'admin');
 END IF;
 END $
-
-DROP TRIGGER IF EXISTS `add_device_user_coin`;
-CREATE TRIGGER add_device_user_coin
-AFTER INSERT ON user FOR EACH ROW
-BEGIN
-    -- 添加新普通用户趣币数量
-    INSERT INTO user_coin(`id`, `user_id`, `coin_num`, `updated_time`, `operator`) values(REPLACE(uuid(), '-', ''), new.id,  0, CURRENT_TIME(), 'admin');
-END $
-
-DROP TRIGGER IF EXISTS `addVideoFavourite`;
-CREATE TRIGGER addVideoFavourite
-AFTER INSERT ON user_favourite FOR EACH ROW
-BEGIN
-    -- 增加收藏数量
-   update video set favorite_count= favorite_count+1 where id = new.video_id;
-END $
-
-DROP TRIGGER IF EXISTS `addVideoThumbsUp`;
-CREATE TRIGGER addVideoThumbsUp
-AFTER INSERT ON user_thumbs_up FOR EACH ROW
-BEGIN
-    -- 增加点赞数量
-   update video set thumbs_up_count= thumbs_up_count+1 where id = new.video_id;
-END $
-
-DROP TRIGGER IF EXISTS `delVideoFavourite`;
-CREATE TRIGGER delVideoFavourite
-AFTER DELETE ON user_favourite FOR EACH ROW
-BEGIN
-    -- 减少收藏数量
-   update video set favorite_count= favorite_count-1 where id = old.video_id;
-END $
-
-DROP TRIGGER IF EXISTS `delVideoThumbsUp`;
-CREATE TRIGGER delVideoThumbsUp
-AFTER DELETE ON user_thumbs_up FOR EACH ROW
-BEGIN
-    -- 减少点赞数量
-   update video set thumbs_up_count= thumbs_up_count-1 where id = old.video_id;
-END $
-
-DROP TRIGGER IF EXISTS `addVideoWatchNum`;
-CREATE TRIGGER addVideoWatchNum
-AFTER INSERT ON watch_record FOR EACH ROW
-BEGIN
-    -- 增加观看数量
-   update video set play_count= play_count+1 where id = new.video_id;
-END $
-
-DROP TRIGGER IF EXISTS `addPageUrl`;
-CREATE TRIGGER addPageUrl
-AFTER INSERT ON red_packet FOR EACH ROW
-BEGIN
-    -- 增加领取地址
-   update red_packet set page_url= CONCAT('https://qpf.lovek12.com/redpacket.html?id=',new.id);
-END
 
 DELIMITER ;
